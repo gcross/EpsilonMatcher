ea8856237a54122f7406374e059855d057afaae8
Gregory Crosswhite <gcross@phys.washington.edu>
2010-03-01 11:23:46 -0800

    Added test case for two elements

--- tests/test.hs
+++ tests/test.hs
@@ -18,10 +18,18 @@ import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
 import Test.QuickCheck
+
+import Debug.Trace
 -- @-node:gcross.20091220080702.1870:<< Import needed modules >>
 -- @nl
 
 -- @+others
+-- @+node:gcross.20100301111615.1274:Functions
+-- @+node:gcross.20100301111615.1275:echo
+echo :: Show a => a -> a
+echo x = trace (show x) x
+-- @-node:gcross.20100301111615.1275:echo
+-- @-node:gcross.20100301111615.1274:Functions
 -- @-others
 
 main = defaultMain
@@ -45,6 +53,20 @@ main = defaultMain
             lookupMatch value
         )
     -- @-node:gcross.20100228202857.1315:singleton case
+    -- @+node:gcross.20100301111615.1273:duo case
+    ,testProperty "duo case" $ \(value1 :: Int) (value2 :: Int) ->
+        (case value1 `compare` value2 of
+            LT -> (((0,1),Map.fromList [(0,0),(1,1)]) ==)
+            GT -> (((0,1),Map.fromList [(0,1),(1,0)]) ==)
+            EQ -> (((0,0),Map.fromList [(0,0)]) ==)
+        )
+        $
+        (runEpsilonMatcher (0 :: Int) $ do
+            key1 <- lookupMatch value1
+            key2 <- lookupMatch value2
+            return (key1,key2)
+        )
+    -- @-node:gcross.20100301111615.1273:duo case
     -- @-others
     -- @-node:gcross.20100228202857.1304:<< Tests >>
     -- @nl
