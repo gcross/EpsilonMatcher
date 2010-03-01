41a6608ab70839cbc9ad5e6ceef0f88a15716c4e
Gregory Crosswhite <gcross@phys.washington.edu>
2010-03-01 10:42:43 -0800

    Added singleton test case.

--- tests/test.hs
+++ tests/test.hs
@@ -2,6 +2,11 @@
 -- @+node:gcross.20091220080702.1868:@thin test.hs
 -- @@language Haskell
 
+-- @<< Language extensions >>
+-- @+node:gcross.20100228202857.1316:<< Language extensions >>
+{-# LANGUAGE ScopedTypeVariables #-}
+-- @-node:gcross.20100228202857.1316:<< Language extensions >>
+-- @nl
 -- @<< Import needed modules >>
 -- @+node:gcross.20091220080702.1870:<< Import needed modules >>
 import Data.EpsilonMatcher
@@ -10,7 +15,9 @@ import qualified Data.Map as Map
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
+import Test.QuickCheck
 -- @-node:gcross.20091220080702.1870:<< Import needed modules >>
 -- @nl
 
@@ -30,6 +37,14 @@ main = defaultMain
             runEpsilonMatcher (0 :: Int) $ do
                 return ()
     -- @-node:gcross.20100228202857.1305:null case
+    -- @+node:gcross.20100228202857.1315:singleton case
+    ,testProperty "singleton case" $ \(value :: Int) ->
+        (0,Map.singleton 0 0)
+        ==
+        (runEpsilonMatcher (0 :: Int) $ do
+            lookupMatch value
+        )
+    -- @-node:gcross.20100228202857.1315:singleton case
     -- @-others
     -- @-node:gcross.20100228202857.1304:<< Tests >>
     -- @nl
